9ff6d8c12b8a6e3c58bff8862ce9b2cfbd71f844
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2020-05-08 17:21:29 +0200

    Revert "core: Use particle pairs in RDF kernel"
    This reverts commit f71695c0f3c2be321f6a7decdaf8b0e94b2ba31f.

--- src/core/observables/RDF.hpp
+++ src/core/observables/RDF.hpp
@@ -38,7 +38,8 @@ class RDF : public Observable {
   std::vector<int> m_ids2;
 
   virtual std::vector<double>
-  evaluate(Utils::Span<const std::pair<const Particle *const, const Particle *const>> particle_pairs) const;
+  evaluate(Utils::Span<const Particle *const> particles1,
+           Utils::Span<const Particle *const> particles2) const;
 
 public:
   // Range of the profile.
