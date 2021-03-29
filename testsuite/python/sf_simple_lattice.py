4a0c3d24cb950bff764bbcecf87076689f199eb7
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2021-03-29 16:18:36 +0200

    tests: Check orthorhombic and tetragonal lattices

--- testsuite/python/sf_simple_lattice.py
+++ testsuite/python/sf_simple_lattice.py
@@ -52,6 +52,27 @@ class StructureFactorTest(ut.TestCase):
         peaks = (wavevectors[np.nonzero(intensities_int)] * 2 / np.pi)**2
         np.testing.assert_array_almost_equal(peaks, peaks_ref)
 
+    def test_cco(self):
+        """Check c-centered orthorhombic lattice"""
+        xen = range(0, self.box_l, 4)
+        for i, j, k in itertools.product(xen, repeat=3):
+            self.system.part.add(type=self.part_ty, pos=(i, j, k))
+        for i, j, k in itertools.product(xen, repeat=3):
+            self.system.part.add(type=self.part_ty, pos=(i + 2, j + 2, k))
+        wavevectors, intensities = self.system.analysis.structure_factor(
+            sf_types=[self.part_ty], sf_order=self.sf_order)
+        intensities_int = np.around(intensities).astype(int)
+        print(intensities)
+        print((wavevectors[np.nonzero(intensities_int)] * 2 / np.pi)**2)
+        np.testing.assert_array_almost_equal(intensities, intensities_int)
+        intensities_ref = np.zeros(intensities.shape)
+        intensities_ref[np.nonzero(intensities_int)] = len(self.system.part)
+        np.testing.assert_array_equal(intensities_int, intensities_ref)
+        # (h+k) even => F = 2f, otherwise F = 0
+        peaks_ref = np.arange(2, self.box_l + 1, 2)
+        peaks = (wavevectors[np.nonzero(intensities_int)] * 2 / np.pi)**2
+        np.testing.assert_array_almost_equal(peaks, peaks_ref)
+
     def test_bcc(self):
         """Check body-centered cubic lattice"""
         xen = range(0, self.box_l, 4)
@@ -89,6 +110,20 @@ class StructureFactorTest(ut.TestCase):
         peaks_ref = [3, 4, 8, 11, 12, 16]
         peaks = (wavevectors[np.nonzero(intensities_int)] * 2 / np.pi)**2
         np.testing.assert_array_almost_equal(peaks, peaks_ref)
+
+    def test_tetragonal(self):
+        """Check tetragonal lattice"""
+        xen = range(0, self.box_l, 1)
+        yen = range(0, self.box_l, 2)
+        zen = range(0, self.box_l, 4)
+        for i, j, k in itertools.product(xen, yen, zen):
+            self.system.part.add(type=self.part_ty, pos=(i, j, k))
+        wavevectors, intensities = self.system.analysis.structure_factor(
+            sf_types=[self.part_ty], sf_order=self.sf_order)
+        intensities_int = np.around(intensities).astype(int)
+        print(intensities_int)
+        print(intensities[np.nonzero(intensities_int)])
+        print((wavevectors[np.nonzero(intensities_int)] * 2 / np.pi)**2)
 
     def test_exceptions(self):
         with self.assertRaisesRegex(ValueError, 'order has to be a strictly positive number'):
