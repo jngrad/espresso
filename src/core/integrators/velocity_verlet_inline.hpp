88b5b94b8e66262604adcbffd9f30eaae0c7c988
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2021-06-23 20:19:44 +0200

    core: Let ParticleRange filter particles

--- src/core/integrators/velocity_verlet_inline.hpp
+++ src/core/integrators/velocity_verlet_inline.hpp
@@ -30,6 +30,11 @@
 
 #include <utils/math/sqr.hpp>
 
+template <Propagation criterion>
+struct PropagationPredicate {
+    bool operator()(Particle const &p) { return p.p.propagation == criterion; };
+};
+
 /** Propagate the velocities and positions. Integration steps before force
  *  calculation of the Velocity Verlet integrator: <br> \f[ v(t+0.5 \Delta t) =
  *  v(t) + 0.5 \Delta t f(t)/m \f] <br> \f[ p(t+\Delta t) = p(t) + \Delta t
@@ -38,7 +43,7 @@
 inline void velocity_verlet_propagate_vel_pos(const ParticleRange &particles) {
 
   auto const skin2 = Utils::sqr(0.5 * skin);
-  for (auto &p : particles) {
+  for (auto &p : particles.filter<PropagationPredicate<Propagation::SYSTEM_DEFAULT>>()) {
 #ifdef ROTATION
     propagate_omega_quat_particle(p, time_step);
 #endif
@@ -69,7 +74,7 @@ inline void velocity_verlet_propagate_vel_pos(const ParticleRange &particles) {
 inline void
 velocity_verlet_propagate_vel_final(const ParticleRange &particles) {
 
-  for (auto &p : particles) {
+  for (auto &p : particles.filter<PropagationPredicate<Propagation::SYSTEM_DEFAULT>>()) {
     // Virtual sites are not propagated during integration
     if (p.p.is_virtual)
       continue;
