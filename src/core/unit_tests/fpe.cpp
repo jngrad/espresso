a6c8b4ed04704d5e40243f7ab8225610253e8179
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2024-12-14 00:29:34 +0100

    arm-specific

--- src/core/unit_tests/fpe.cpp
+++ src/core/unit_tests/fpe.cpp
@@ -90,6 +90,24 @@ fe_trap::fe_trap(std::optional<int> excepts, bool unique) {
   assert(status == 0);
 #endif // __GLIBC__
 #endif // __STDC_IEC_559__
+#if defined(__arm64__) and defined(__APPLE__)
+#define ESPRESSO_FENV_DEFINED_CTOR
+{
+   m_flags = parse_excepts(excepts);
+printf("%i -> %i\n", __fpcr_trap_invalid, FE_INVALID<<8);
+printf("%i -> %i\n", __fpcr_trap_divbyzero, FE_DIVBYZERO<<8);
+printf("%i -> %i\n", __fpcr_trap_overflow, FE_OVERFLOW<<8);
+printf("%i -> %i\n", __fpcr_trap_underflow, FE_UNDERFLOW<<8);
+//   m_flags = __fpcr_trap_divbyzero | __fpcr_trap_invalid | __fpcr_trap_overflow | __fpcr_trap_underflow ;
+    std::fenv_t env;
+    auto ret1 = std::fegetenv(&env);
+assert(ret1 == 0);
+printf("%i : %i  \n", FE_INVALID, __fpcr_trap_invalid);
+ env.__fpcr |= m_flags;
+  auto ret2 =  std::fesetenv(&env);
+assert(ret2 == 0);
+}
+#endif
   m_unique = unique;
 
   // sentinel if no implementation-defined solution was found
@@ -107,6 +125,20 @@ fe_trap::~fe_trap() {
   assert(status == 0 or status == m_flags);
 #endif // __GLIBC__
 #endif // __STDC_IEC_559__
+#if defined(__arm64__) and defined(__APPLE__)
+#define ESPRESSO_FENV_DEFINED_DTOR
+{
+
+    std::fenv_t env;
+    auto ret1 = std::fegetenv(&env);
+assert(ret1 == 0);
+printf("disable: %i\n",env.__fpcr);
+assert((env.__fpcr & m_flags) == m_flags);
+ env.__fpcr &= ~m_flags;
+  auto ret2 =  std::fesetenv(&env);
+assert(ret2 == 0);
+}
+#endif
 
   // sentinel if no implementation-defined solution was found
 #if defined(ESPRESSO_FENV_DEFINED_DTOR)
@@ -120,9 +152,10 @@ int fe_trap::parse_excepts(std::optional<int> excepts) {
 #if defined(__STDC_IEC_559__)
 #if defined(__GLIBC__)
 #define ESPRESSO_FENV_DEFINED_CTOR
-  retval = excepts ? *excepts : (FE_DIVBYZERO | FE_INVALID | FE_OVERFLOW);
+  retval = excepts ? *excepts : (FE_DIVBYZERO | FE_INVALID | FE_OVERFLOW | FE_UNDERFLOW);
 #endif // __GLIBC__
 #endif // __STDC_IEC_559__
+  retval = excepts ? *excepts : (FE_DIVBYZERO | FE_INVALID | FE_OVERFLOW | FE_UNDERFLOW)<<8; // __fpcr_trap_divbyzero | __fpcr_trap_invalid | __fpcr_trap_overflow | __fpcr_trap_underflow ;
   return retval;
 }
 
@@ -144,6 +177,7 @@ fe_trap::scoped_instance fe_trap::make_shared_scoped(std::optional<int> excepts)
       throw std::runtime_error("Cannot create more than 1 instance of fe_trap");
     }
     if (watched->get_flags() != parse_excepts(excepts)) {
+printf("%i != %i \n", watched->get_flags(), parse_excepts(excepts));
       throw std::invalid_argument("Cannot mix different exceptions with fe_trap");
     }
     return fe_trap::scoped_instance(watched);
@@ -159,24 +193,34 @@ fe_trap::scoped_instance fe_trap::make_shared_scoped(std::optional<int> excepts)
 #include <cstdio>
 #include <cfenv>
 
-#if defined(__STDC_IEC_559__) and defined(__GLIBC__) and !defined(__FAST_MATH__)
-#include <boost/test/unit_test.hpp>
-
+#if defined(__arm64__) and defined(__APPLE__)
+//#if defined(__STDC_IEC_559__) and defined(__GLIBC__) and !defined(__FAST_MATH__)
+//#include <boost/test/unit_test.hpp>
+#define BOOST_CHECK_EQUAL(x, y) if (x != y) printf("%i != %i\n", x, y);
+#define BOOST_REQUIRE_EQUAL(x, y) if (x != y) printf("%i != %i\n", x, y);
+#define BOOST_CHECK(x) if (not x) printf("%i\n", (int)(x));
+#define BOOST_REQUIRE(x) if (not x) printf("%i\n", (int)(x));
 #include <cassert>
 #include <cmath>
 #include <csetjmp>
 #include <csignal>
 #include <initializer_list>
 
+#ifdef SIGFPE
+#undef SIGFPE
+#define SIGFPE SIGILL
+#endif
+
 static volatile std::sig_atomic_t last_signal_status;
 static std::jmp_buf jmp_env;
- 
+
 void sigfpe_handler(int signal) {
   ::last_signal_status = signal;
   siglongjmp(::jmp_env, 1);
 }
 
-BOOST_AUTO_TEST_CASE(trap_by_signal) {
+//BOOST_AUTO_TEST_CASE(trap_by_signal) {
+int main()  {
   double volatile denominator = 0.;
   double volatile value = 1.;
   printf("SIGILL: %i\n", SIGILL);
@@ -186,7 +230,7 @@ BOOST_AUTO_TEST_CASE(trap_by_signal) {
   BOOST_REQUIRE_EQUAL(::last_signal_status, 0);
   [[maybe_unused]] auto sig_ret = std::signal(SIGFPE, sigfpe_handler);
   assert(sig_ret != SIG_ERR);
-  std::signal(SIGILL, sigfpe_handler);
+  //std::signal(SIGILL, sigfpe_handler);
     {
       auto const trap = fe_trap::make_unique_scoped();
       BOOST_REQUIRE(trap.is_unique());
@@ -232,8 +276,29 @@ BOOST_AUTO_TEST_CASE(trap_by_signal) {
   std::signal(SIGFPE, SIG_DFL);
   std::signal(SIGILL, SIG_DFL);
   value = 1. / denominator;
-}
 
+{
+    auto const trap = fe_trap::make_unique_scoped();
+//    fe_trap::make_unique_scoped();
+//BOOST_REQUIRE_THROW(fe_trap::make_unique_scoped(), std::runtime_error);
+//fe_trap::make_shared_scoped();
+  //  BOOST_REQUIRE_THROW(fe_trap::make_shared_scoped(), std::runtime_error);
+  }
+  {
+    auto const trap = fe_trap::make_shared_scoped();
+    //BOOST_REQUIRE_THROW(fe_trap::make_unique_scoped(), std::runtime_error);
+    for (int other_excepts : {FE_INEXACT, FE_ALL_EXCEPT}) {
+      if (trap.get_flags() != other_excepts) {
+//fe_trap::make_shared_scoped(FE_ALL_EXCEPT);
+       // BOOST_REQUIRE_THROW(fe_trap::make_shared_scoped(FE_ALL_EXCEPT),
+         //                   std::invalid_argument);
+      }
+    }
+  }
+
+puts("out");
+}
+/*
 BOOST_AUTO_TEST_CASE(exceptions) {
   {
     auto const trap = fe_trap::make_unique_scoped();
@@ -251,8 +316,10 @@ BOOST_AUTO_TEST_CASE(exceptions) {
     }
   }
 }
+*/
 #else
 #include <cstdio>
+#include <cfenv>
 int main() {
 #if defined(__STDC_IEC_559__)
 puts("defined(__STDC_IEC_559__)");
@@ -268,6 +335,26 @@ puts("!defined(__GLIBC__)");
 puts("defined(__FAST_MATH__)");
 #else
 puts("!defined(__FAST_MATH__)");
+#endif
+#if defined(__arm64__)
+puts("defined(__arm64__)");
+#else
+puts("!defined(__arm64__)");
+#endif
+#if defined(__APPLE__)
+puts("defined(__APPLE__)");
+#else
+puts("!defined(__APPLE__)");
+#endif
+#if defined(HAVE_FPCR)
+puts("defined(HAVE_FPCR)");
+#else
+puts("!defined(HAVE_FPCR)");
+#endif
+#if defined(HAVE_FEENABLEEXCEPT)
+puts("defined(HAVE_FEENABLEEXCEPT)");
+#else
+puts("!defined(HAVE_FEENABLEEXCEPT)");
 #endif
 return 2;
 }
