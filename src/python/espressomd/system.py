052f3c9e9344c2a2537b21841d1555ad72c92fc2
Jean-Noël Grad <jgrad@icp.uni-stuttgart.de>
2026-05-07 20:56:12 +0200

    step

--- src/python/espressomd/system.py
+++ src/python/espressomd/system.py
@@ -327,5 +327,5 @@ class System(ScriptInterfaceHelper):
         assert_features("EXCLUSIONS")
         self.part.auto_exclusions(distance=distance)
 
-    def setup_type_map(self):
+    def setup_type_map(self, *args, **kwargs):
         print("system.setup_type_map() is deprecated and no longer has any effect")
